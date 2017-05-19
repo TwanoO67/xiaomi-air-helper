d5067de779e6161a8d631c3160e9d5278836b513
WEBER Antoine <perso@weberantoine.fr>
2017-05-19 11:38:25 +0200

    ajout du transfert audio vers hdmi

--- screen.sh
+++ screen.sh
@@ -3,12 +3,16 @@
 
 xrandr --listactivemonitors | grep "Monitors: 2" && {
   xrandr --output eDP-1 --off
-  echo "Disabled screen"
+  echo "Mode ecran externe"
+  pacmd set-card-profile 0 output:hdmi-stereo+input:analog-stereo
+  xrandr --output DP-1 --scale 0.7x0.7
   exit
 }
 
 xrandr --listactivemonitors | grep "Monitors: 1" && {
   xrandr --output eDP-1 --preferred --right-of DP-1
-  echo "Enabling screen"
+  xrandr --output DP-1 --scale 1x1
+  pacmd set-card-profile 0 output:analog-stereo+input:analog-stereo
+  echo "Mode double ecran"
   exit
 }
